0e5656e90f8b541ca8b4db382f0bf9d66967c1f3
ThisaraWeerakoon <thisara.weerakoon2001@gmail.com>
2024-07-07 23:59:29 +0530

    changed

--- src/main/java/com/example/ticketBooking/TicketBookingApplication.java
+++ src/main/java/com/example/ticketBooking/TicketBookingApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketBookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketBookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketBookingApplication.class, args);
+        System.out.print(("HelloWorld"));
+    }
 
 }
