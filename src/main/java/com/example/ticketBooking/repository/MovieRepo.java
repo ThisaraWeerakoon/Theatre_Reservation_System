60e3d76f4473b2e477c54e06e791a9ee8271bf6d
ThisaraWeerakoon <thisara.weerakoon2001@gmail.com>
2024-08-20 23:23:50 +0530

    dfdfd

--- src/main/java/com/example/ticketBooking/repository/MovieRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.ticketBooking.repository;
-import com.example.ticketBooking.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepo extends JpaRepository<Movie, Long> {
-}
