4ab414e2ea4b40dc825fdb4a2c46d6aa3f1f0e97
ThisaraWeerakoon <thisara.weerakoon2001@gmail.com>
2024-08-22 17:49:02 +0530

    updaed

--- src/main/java/com/tui/Theatre_Reservation_System/repositories/MovieRepository.java
+++ src/main/java/com/tui/Theatre_Reservation_System/repositories/MovieRepository.java
@@ -3,6 +3,7 @@ package com.tui.Theatre_Reservation_System.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie,Integer> {
+    
 
 
 }
