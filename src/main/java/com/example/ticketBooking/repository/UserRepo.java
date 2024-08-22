004bb0695d6e934b02dfc51f9c7a1216bf9316f9
Thisara Weerakoon <83450623+ThisaraWeerakoon@users.noreply.github.com>
2024-08-22 17:37:23 +0530

    Delete src directory

--- src/main/java/com/example/ticketBooking/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ticketBooking.repository;
-import com.example.ticketBooking.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-
-    User findByEmail(String email);
-}
